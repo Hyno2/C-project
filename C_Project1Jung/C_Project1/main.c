c90313ea729a231852575f6ccc138bce0ab563e9
In-hyuk <dlsgur109@naver.com>
2023-09-13 23:17:45 +0900

    기능추가

--- C_Project1Jung/C_Project1/main.c
+++ C_Project1Jung/C_Project1/main.c
@@ -1,61 +1,228 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <windows.h>
+#include <stdlib.h>	// exit()
+#include <time.h>	// time()
+#include <string.h>	// strcpy()
+#include <conio.h>
+#include <windows.h>// Sleep()
+#include "menu.h"
+#include "ui.h"
 
-typedef enum {
-	NOCURSOR,		// 커서 없앰		0
-	SOLID_CURSOR,	// solid 형태	1
-	NORMAL_CURSOR	// 일반 형태		2
-} CURSOR_TYPE;
+typedef struct _income {
+	int month;
+	int day;
+	int money;
+	char memo[50];
+} income;
 
-typedef enum {
-	BLACK,	// 0
-	BLUE,	// 1
-	GREEN,	// 2
-	CYAN,
-	RED,
-	PURPLE,
-	YELLOW,
-	WHITE,
-	GRAY,
-	LIGHT_BLUE,
-	LIGHT_GREEN,
-	LIGHT_CYAN,
-	LIGHT_RED,
-	LIGHT_PURPLE,
-	LIGHT_YELLOW,
-	LIGHT_GRAY	// 15
-} FONT_COLOR;
-
-void title();
-void gotoxy(int x, int y);
-void set_cursor_type(CURSOR_TYPE type);
-void font_color(FONT_COLOR color);
-
+typedef struct _out {
+	int month;
+	int day;
+	int money;
+	char type[20];
+	char pay[20];
+	char memo[50];
+} out;
 
 int main() {
 	set_cursor_type(NOCURSOR);
 	font_color(LIGHT_GREEN);
 	title();
 	_getch();
-
 	system("cls");
 	font_color(LIGHT_GRAY);
 	set_cursor_type(NORMAL_CURSOR);
 	while (1) {
 		switch (main_menu()) {
-		case 1:
+		case 1: {
+			income in = { 0 };
+			printf("날짜 입력 (9월 5일 일시 ex.9 5): ");
+			scanf("%d %d", &in.month, &in.day);
+			printf("금액 입력(원 단위): ");
+			scanf("%d", &in.money);
+			printf("메모 입력: ");
+			scanf("%s", in.memo);
+			
+			FILE* fp = fopen("income", "ab");
+			fwrite(&in, sizeof(income), 1, fp);
+			fclose(fp);
+			printf("저장 완료!\n");
+			system("pause");
+		}
 			break;
 		case 2: {
+			out ou = { 0 };
+			printf("날짜 입력 (9월 5일 일시 ex.9 5): ");
+			scanf("%d %d", &ou.month, &ou.day);
+			printf("지출 금액(원 단위): ");
+			scanf("%d", &ou.money);
 			system("cls");
-			int submenu1 = search_menu();
+			int submenu0 = type_menu();
+			if (submenu0 == 1) {
+				strcpy(ou.type, "식비");
+			}
+			else if (submenu0 == 2) {
+				strcpy(ou.type, "주거 / 통신");
+			}
+			else if (submenu0 == 3) {
+				strcpy(ou.type, "의복 / 미용");
+			}
+			else if (submenu0 == 4) {
+				strcpy(ou.type, "건강 / 문화");
+			}
+			else if (submenu0 == 5) {
+				strcpy(ou.type, "교육 / 육아");
+			}
+			else if (submenu0 == 6) {
+				strcpy(ou.type, "교통 / 차량");
+			}
+			else if (submenu0 == 7) {
+				strcpy(ou.type, "기타");
+			}
+			printf("메모 입력 (ex.주류비,통신비 등등): ");
+			scanf("%s", ou.memo);
+			system("cls");
+			int submenu1 = import_menu();
+			if (submenu1 == 1) {
+				strcpy(ou.pay, "카드");
+			}
+			else if (submenu1 == 2) {
+				strcpy(ou.pay, "현금");
+			}
+			FILE* fp = fopen("out", "ab");
+			fwrite(&ou, sizeof(out), 1, fp);
+			fclose(fp);
+			printf("저장 완료!\n");
+			system("pause");
 		}
 			break;
 		case 3:
 			system("cls");
-			int submenu2 = sub_menu();
+			int submenu2 = check_menu();
+			if (submenu2 == 1) {
+				int count = 1;
+				int cnt = 1;
+				int totalincome = 0;
+				int totalout = 0;
+				income in = { 0 };
+				out ou = { 0 };
+				FILE* fp1 = fopen("income", "rb");
+				FILE* fp2 = fopen("out", "rb");
+				int day = 0;
+				printf("궁금한 날을 입력해주세용 : ");
+				scanf("%d", &day);
+				while (fread(&in, sizeof(income), 1, fp1) > 0) {
+					if (day == in.day) {
+						totalincome += in.money;
+						printf("수익내역 %d 번째\n", count++);
+						printf("날짜: %d월 %d일\n", in.month, in.day);
+						printf("금액: %d\n", in.money);
+						printf("메모: %s\n", in.memo);
+						printf("-----------------\n");
+					}
+				}
+				while (fread(&ou, sizeof(out), 1, fp2) > 0) {
+					if (day == ou.day) {
+						totalout += ou.money;
+						printf("지출내역 %d 번째\n", cnt++);
+						printf("날짜: %d월 %d일\n", ou.month, ou.day);
+						printf("금액: %d원\n", ou.money);
+						printf("타입: %s\n", ou.type);
+						printf("메모: %s\n", ou.memo);
+						printf("결제수단: %s\n", ou.pay);
+						printf("-----------------\n");
+					}
+				}
+				fclose(fp1);
+				fclose(fp2);
+				printf("%d일 총 수익 금액: %d원, 총 지출 금액: %d원\n",day, totalincome, totalout);
+				printf("%d일 총 합산 금액: %d원\n",day, totalincome - totalout);
+				system("pause");
+			}
+			if (submenu2 == 2) {
+				int count = 1;
+				int cnt = 1;
+				int totalincome = 0;
+				int totalout = 0;
+				income in = { 0 };
+				out ou = { 0 };
+				FILE* fp1 = fopen("income", "rb");
+				FILE* fp2 = fopen("out", "rb");
+				int month = 0;
+				printf("몇월 내역을 출력하시겠습니까? : ");
+				scanf("%d", &month);
+				while (fread(&in, sizeof(income), 1, fp1) > 0) {
+					if (month == in.month) {
+						totalincome += in.money;
+						printf("수익내역 %d 번째\n", count++);
+						printf("날짜: %d월 %d일\n", in.month, in.day);
+						printf("금액: %d\n", in.money);
+						printf("메모: %s\n", in.memo);
+						printf("-----------------\n");
+					}
+				}
+				while (fread(&ou, sizeof(out), 1, fp2) > 0) {
+					if (month == ou.month) {
+						totalout += ou.money;
+						printf("지출내역 %d 번째\n", cnt++);
+						printf("날짜: %d월 %d일\n", ou.month, ou.day);
+						printf("금액: %d원\n", ou.money);
+						printf("타입: %s\n", ou.type);
+						printf("메모: %s\n", ou.memo);
+						printf("결제수단: %s\n", ou.pay);
+						printf("-----------------\n");
+					}
+				}
+				fclose(fp1);
+				fclose(fp2);
+				printf("%d달 총 수익 금액: %d원, 총 지출 금액: %d원\n", month, totalincome, totalout);
+				printf("%d달 총 합산 금액: %d원\n", month, totalincome - totalout);
+				system("pause");
+			}
+
+			if (submenu2 == 3) {
+				int count = 1;
+				int cnt = 1;
+				int totalincome = 0;
+				int totalout = 0;
+				income in = { 0 };
+				out ou = { 0 };
+				FILE* fp1 = fopen("income", "rb");
+				FILE* fp2 = fopen("out", "rb");
+				while (fread(&in, sizeof(income), 1, fp1) > 0) {
+					totalincome += in.money;
+					printf("수익내역 %d 번째\n",count++);
+					printf("날짜: %d월 %d일\n",in.month, in.day);
+					printf("금액: %d원\n", in.money);
+					printf("메모: %s\n", in.memo);
+					printf("-----------------\n");
+				}
+				while (fread(&ou, sizeof(out), 1, fp2) > 0) {
+					totalout += ou.money;
+					printf("지출내역 %d 번째\n",cnt++);
+					printf("날짜: %d월 %d일\n",ou.month, ou.day);
+					printf("금액: %d원\n", ou.money);
+					printf("타입: %s\n", ou.type);
+					printf("메모: %s\n", ou.memo);
+					printf("결제수단: %s\n", ou.pay);
+					printf("-----------------\n");
+				}
+				fclose(fp1);
+				fclose(fp2);
+				printf("총 수익 금액: %d원, 총 지출 금액: %d원\n", totalincome, totalout);
+				printf("총 합산 금액: %d원\n", totalincome - totalout);
+				system("pause");
+			}
 			break;
-		case 4:
+		case 4: // 서브메뉴 더 받아서 전체내역 삭제할지, 검색해서 삭제?? , 아니면 수익내역만 삭제 , 지출내역만 삭제 이런거 해도될듯
+		{
+			FILE* fp1 = fopen("income", "wb");
+			FILE* fp2 = fopen("out", "wb");
+			fclose(fp1);
+			fclose(fp2);
+		}
+			break;
+		case 5:
 			printf("프로그램을 종료합니다.\n");
 			printf("부자 되세요~");
 			exit(0);
@@ -64,102 +231,4 @@ int main() {
 	}
 }
 
-int main_menu()
-{
-	printf("=============================\n");
-	printf("         가 계 부\n");
-	printf("=============================\n");
-	printf("1.수입\n");
-	printf("2.지출\n");
-	printf("3.조회\n");
-	printf("4.종료\n");
-	printf("=============================\n");
-	printf("메뉴 선택:");
-	int menu;
-	scanf("%d", &menu);
-	return menu;
-}
 
-int search_menu()
-{
-	printf("=============================\n");
-	printf("분류 항목 선택\n");
-	printf("=============================\n");
-	printf("1.식비\n");
-	printf("2.주거 / 통신\n");
-	printf("3.의복 / 미용\n");
-	printf("4.건강 / 문화\n");
-	printf("5.교육 / 육아\n");
-	printf("6.교통 / 차량\n");
-	printf("7.기타\n");
-	printf("8.나가기\n");
-	printf("=============================\n");
-	printf("입력 번호 :");
-	int menu;
-	scanf("%d", &menu);
-	return menu;
-}
-
-int sub_menu()
-{
-	printf("=============================\n");
-	printf("1.일별로 조회\n");
-	printf("2.월별로 조회\n");
-	printf("3.전체내역 조회\n");
-	printf("4.나가기\n");
-	printf("=============================\n");
-	int menu;
-	scanf("%d", &menu);
-	return menu;
-}
-
-void title()
-{
-	int x = 5;
-	int y = 3;
-	gotoxy(x, y + 0); printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
-	gotoxy(x, y + 1); printf("■							     ■");
-	gotoxy(x, y + 2); printf("■							     ■");
-	gotoxy(x, y + 3); printf("■							     ■");
-	gotoxy(x, y + 4); printf("■							     ■");
-	gotoxy(x, y + 5); printf("■							     ■");
-	gotoxy(x, y + 6); printf("■							     ■");
-	gotoxy(x, y + 7); printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
-	gotoxy(x + 12, y + 2); printf("가 계 부");
-	gotoxy(x + 33, y + 4); printf("만든날짜	: 2023.09.13");
-	gotoxy(x, y + 12); printf("메인화면 이동은 아무키나 누르세요!!!");
-}
-
-void gotoxy(int x, int y)
-{
-	COORD pos = { x, y };
-	// 윈도우 콘솔에서 커서 위치 이동
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-}
-
-void set_cursor_type(CURSOR_TYPE type)
-{
-	CONSOLE_CURSOR_INFO info = { 0 };
-	switch (type) {
-	case NOCURSOR:
-		info.dwSize = 1;
-		info.bVisible = FALSE;
-		break;
-	case SOLID_CURSOR:
-		info.dwSize = 100;
-		info.bVisible = TRUE;
-		break;
-	case NORMAL_CURSOR:
-		info.dwSize = 20;
-		info.bVisible = TRUE;
-		break;
-	}
-	// 커서 타입을 설정 (win32 API)
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
-}
-
-void font_color(FONT_COLOR color)
-{
-	// 폰트에 색생 적용
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
